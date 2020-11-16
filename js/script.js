cb1aeb9371a3252b17cca1c54d226b1e182bc7fd
adityaafin <74429594+adityaafin@users.noreply.github.com>
2020-11-16 21:54:32 +0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,84 @@
+/*Code By : https://codepen.io/simoami/pen/zstvo*/
+
+$(function() {
+  var data = [
+  { 
+    action: 'type',
+    strings: ["Hai apa kabar kamu"],
+    output: '<span class="gray">root@nulled</span><br>&nbsp;',
+    postDelay: 2000
+  },
+  { 
+    action: 'type',
+    strings: ["ls -la"],
+    output: '<span class="gray">secret &nbsp;&nbsp; <i class="fa fa-heart"></i> Windi <i class="fa fa-heart"></i></span><br>&nbsp;',
+    postDelay: 2000
+  },
+  { 
+    action: 'type',
+    strings: ["cd secret^400"],
+    output: '<span class="gray">secret &nbsp;&nbsp; <i class=""></i> you so beautiful <i class=""></i></span><br>&nbsp;',
+    postDelay: 2000
+  },
+   { 
+    action: 'type',
+    strings: ["ls"],
+    output: '<span class="gray">data.txt</span><br>&nbsp;',
+    postDelay: 2000
+  },
+  { 
+    action: 'type',
+    clear: true,
+    strings: ['cat data.txt^400'],
+    output: $('.mimik-run-output').html()
+  },
+  { 
+    action: 'type',
+    strings: ["Let's Work And Collaboration : afin@students.universitasmulia.ac.id"],
+    postDelay: 2000
+  }
+  
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html(''); 
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 30,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
